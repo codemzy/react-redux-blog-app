19bb093b8d8ab489edd9ae05f9c2a24a88d3a872
Emma J <codemzy@gmail.com>
2016-08-16 14:31:55 +0000

    Added Nav to top in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <div>React simple starter</div>
-        {this.props.children}
+        <nav className="navbar navbar-fixed-top navbar-light bg-faded">
+          <Link to='/' className="navbar-brand">Simple Blog with ReactJS and Redux</Link>
+        </nav>
+        <div className="container">
+          {this.props.children}
+        </div>
       </div>
     );
   }
