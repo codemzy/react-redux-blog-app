6ecf6dbcfb377b426c8ff54bf972aafc59db48c3
Emma J <codemzy@gmail.com>
2016-08-16 10:55:19 +0000

    Wired up fetchPosts to PostsIndex

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const KEY = '?key=myfakekey123';
 
 export function fetchPosts() {
--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-    return <div>List of blog posts</div>;
-};
+// actions
+import { fetchPosts } from '../actions/index.js';
+
+
+class PostsIndex extends React.Component {
+    
+    componentWillMount() {
+        this.props.dispatch(fetchPosts());
+    }
+    
+    render() {
+        return (
+            <div>List of blog posts</div>
+        );
+    }
+}
+
+export default connect()(PostsIndex);
