079e9e43ad3a4915d991737c9835ba6245d916bb
Emma J <codemzy@gmail.com>
2016-08-16 12:48:54 +0000

    Added validation to all form fields

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -13,18 +13,20 @@ class PostsNew extends React.Component {
         return (
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create a new post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
-                    <div className="text-help">{title.touched ? title.error : '' }</div>
+                    <div className="text-help form-control-label">{title.touched ? title.error : '' }</div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
+                    <div className="text-help form-control-label">{categories.touched ? categories.error : '' }</div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea className="form-control"{...content} />
+                    <div className="text-help form-control-label">{content.touched ? content.error : '' }</div>
                 </div>
                 <button type="submit" className="btn btn-primary">Add Post</button>
             </form>
@@ -36,6 +38,12 @@ function validate(values) {
     const ERRORS = {};
     if (!values.title) {
         ERRORS.title = 'Enter a blog title';
+    }
+    if (!values.categories) {
+        ERRORS.categories = 'Enter a categories';
+    }
+    if (!values.content) {
+        ERRORS.content = 'Enter some content';
     }
     return ERRORS;
 }
