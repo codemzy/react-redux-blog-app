3401e0ca44682d85eb946f24774044e60fdf376b
Emma J <codemzy@gmail.com>
2016-08-16 13:55:37 +0000

    Added reducer for FETCH_POST action

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,14 @@
 
-import {FETCH_POSTS} from '../actions/index.js';
+import {FETCH_POSTS, FETCH_POST} from '../actions/index.js';
 
 const INITIAL_STATE = { all: [], post: false };
 
 export default function(state = INITIAL_STATE, action) {
     if (action.type === FETCH_POSTS) {
         return { ...state, all: action.payload.data };
+    }
+    if (action.type === FETCH_POST) {
+        return { ...state, post: action.payload.data };
     }
     return state;
 }
