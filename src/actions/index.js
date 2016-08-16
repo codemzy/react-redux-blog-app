1d2d869b1de9a846cf8537aad98b3521be42f17c
Emma J <codemzy@gmail.com>
2016-08-16 12:17:17 +0000

    Added action creator for createPost

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const KEY = '?key=myfakekey123';
@@ -9,6 +10,14 @@ export function fetchPosts() {
     let request = axios.get(`${ROOT_URL}/posts${KEY}`);
     return {
         type: FETCH_POSTS,
+        payload: request
+    };
+}
+
+export function createPost(props) {
+    let request = axios.post(`${ROOT_URL}/posts${KEY}`, props);
+    return {
+        type: CREATE_POST,
         payload: request
     };
 }
